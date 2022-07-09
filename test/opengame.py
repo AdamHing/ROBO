9ccdadc9d317ebda5cbb8ff1ab696619ea673bb0
AdamHing <46699349+AdamHing@users.noreply.github.com>
2022-07-09 12:56:14 -0700

    Delete test directory

--- test/opengame.py
+++
@@ -1,9 +0,0 @@
-
-
-import random
-
-
-BrowserID = random.randint(10000000000, 99999999999)
-
-#somehow this opens up a roblox session 
-webbrowser.open(f"roblox-player:1+launchmode:play+gameinfo:{Gamesession}+launchtime:{int(time()*1000)}+placelauncherurl:https%3A%2F%2Fassetgame.roblox.com%2Fgame%2FPlaceLauncher.ashx%3Frequest%3DRequestGame%26browserTrackerId%3D{BrowserID}%26placeId%3D{PlaceId}%26isPlayTogetherGame%3Dfalse+browsertrackerid:{BrowserID}+robloxLocale:en_us+gameLocale:en_us")
